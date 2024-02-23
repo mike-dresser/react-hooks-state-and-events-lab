fbb4e9384a3d42cc56539d39fb9aa6d88f5052b5
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-22 21:23:33 -0500

    Works fine, does not pass tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Item({ updateCart, cart, name, category }) {
   function handleCartButton() {
-    if (!cart.includes(name)) {
+    if (cart.indexOf(name) === -1) {
       updateCart([...cart, name]);
     } else {
       let filteredCart = cart.filter((item) => item !== name);
@@ -10,12 +10,19 @@ function Item({ updateCart, cart, name, category }) {
     }
   }
 
+  function isInCart(name) {
+    return cart.indexOf(name) !== -1;
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button onClick={handleCartButton} className="add">
-        {cart.includes(name) ? 'Remove from Cart' : 'Add to Cart'}
+      <button
+        onClick={handleCartButton}
+        className={isInCart(name) ? 'add in-cart' : 'add'}
+      >
+        {isInCart(name) ? 'Remove from Cart' : 'Add to Cart'}
       </button>
     </li>
   );
